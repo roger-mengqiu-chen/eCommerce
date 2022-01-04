bace91974cfa4ca9321e613cafbe50055bd0b2a3
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-04 11:35:10 -0700

    update

--- src/main/java/com/example/eComPractice/security/JWTAuthenticationVerficationFilter.java
+++ src/main/java/com/example/eComPractice/security/JWTAuthenticationVerficationFilter.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
-@Component
+
 public class JWTAuthenticationVerficationFilter extends BasicAuthenticationFilter {
 	
 	public JWTAuthenticationVerficationFilter(AuthenticationManager authManager) {
