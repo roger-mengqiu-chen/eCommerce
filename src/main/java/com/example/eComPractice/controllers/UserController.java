d41b34da402d389b3922301540b34be15a2e3cc0
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-04 11:21:06 -0700

    Add log info

--- src/main/java/com/example/eComPractice/controllers/UserController.java
+++ src/main/java/com/example/eComPractice/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.eComPractice.model.persistence.User;
 import com.example.eComPractice.model.persistence.repositories.CartRepository;
 import com.example.eComPractice.model.persistence.repositories.UserRepository;
 import com.example.eComPractice.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +25,8 @@ public class UserController {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
@@ -38,13 +42,16 @@ public class UserController {
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		log.info ("User name set with {}", createUserRequest.getUsername());
 		Cart cart = new Cart();
+
 		cartRepository.save(cart);
 		user.setCart(cart);
 		if(createUserRequest.getPassword().length()<7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
 			//System.out.println("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create ",
 			//		createUserRequest.getUsername());
+			log.error("Error with user password. Cannot create user {}", createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
