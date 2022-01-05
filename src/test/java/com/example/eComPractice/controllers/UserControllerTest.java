f5c554ee3253e4713a6e293d0f472a9566f4b680
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-04 18:07:45 -0700

    Add loginTest

--- src/test/java/com/example/eComPractice/controllers/UserControllerTest.java
+++ src/test/java/com/example/eComPractice/controllers/UserControllerTest.java
@@ -5,16 +5,31 @@ import com.example.eComPractice.model.persistence.User;
 import com.example.eComPractice.model.persistence.repositories.CartRepository;
 import com.example.eComPractice.model.persistence.repositories.UserRepository;
 import com.example.eComPractice.model.requests.CreateUserRequest;
+import com.example.eComPractice.model.requests.LoginRequest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
     private UserController userController;
@@ -24,6 +39,10 @@ public class UserControllerTest {
     private CartRepository cartRepository = mock(CartRepository.class);
 
     private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Before
     public void setup() throws NoSuchFieldException, IllegalAccessException {
@@ -54,5 +73,24 @@ public class UserControllerTest {
         assertEquals("testpassword", u.getPassword());
 
 
+    }
+    @Test
+    public void loginTest() {
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("password");
+        r.setPassword("password");
+        userController.createUser(r);
+        String json = "{\"username\":\"test\",\"password\":\"password\"}";
+
+        try {
+            mockMvc.perform(
+                    MockMvcRequestBuilders.post("/login")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json)
+            ).andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
