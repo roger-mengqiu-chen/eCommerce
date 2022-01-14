a7bbbdbbf3ec097ae9c337d0c507547101d0c4de
Roger Chen <mengqiu.chen@hotmail.com>
2022-01-13 18:20:17 -0700

    Update

--- src/test/java/com/example/eComPractice/controllers/UserControllerTest.java
+++ src/test/java/com/example/eComPractice/controllers/UserControllerTest.java
@@ -5,32 +5,24 @@ import com.example.eComPractice.model.persistence.User;
 import com.example.eComPractice.model.persistence.repositories.CartRepository;
 import com.example.eComPractice.model.persistence.repositories.UserRepository;
 import com.example.eComPractice.model.requests.CreateUserRequest;
-import com.example.eComPractice.model.requests.LoginRequest;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 public class UserControllerTest {
     @Autowired
     private UserController userController;
@@ -47,7 +39,7 @@ public class UserControllerTest {
 
     @Before
     public void setup() throws NoSuchFieldException, IllegalAccessException {
-
+        userController = new UserController();
         TestUtils.injectObjects(userController, "userRepository", userRepository);
         TestUtils.injectObjects(userController, "cartRepository", cartRepository);
         TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
@@ -69,10 +61,42 @@ public class UserControllerTest {
 
         User u = response.getBody();
         assertNotNull(u);
-        assertEquals(1, u.getId());
+        assertEquals(0, u.getId());
         assertEquals("test", u.getUsername());
         assertEquals("testpassword", u.getPassword());
 
     }
 
+    @Test
+    public void create_user_wrong_path() throws Exception {
+        when(encoder.encode("testpassword")).thenReturn("thisIsHashed");
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("testpassword");
+        r.setConfirmPassword("testpassword1");
+
+        final ResponseEntity<User> response = userController.createUser(r);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void findUserByNameHappyPath() {
+        User user = new User();
+        user.setUsername("test");
+        user.setId(1L);
+
+        when (userRepository.findByUsername(anyString())).thenReturn(user);
+        ResponseEntity<User> response = userController.findByUserName(user.getUsername());
+
+        assertEquals(200, response.getStatusCodeValue());
+
+        User foundUser = response.getBody();
+        assertNotNull(foundUser);
+        assertEquals(user.getUsername(), foundUser.getUsername());
+    }
+
+    public void findUserByNameFailure() {
+        ResponseEntity<User> response = userController.findByUserName("ttttt");
+        assertEquals(404, response.getStatusCodeValue());
+    }
 }
